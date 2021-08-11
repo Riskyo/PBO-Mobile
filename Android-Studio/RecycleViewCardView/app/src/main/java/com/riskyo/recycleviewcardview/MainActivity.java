e8cdeceecfa56c65671aff2f903aca332a8e8f9d
Riskyo <68279330+Riskyo@users.noreply.github.com>
2021-08-11 23:16:24 +0700

    Minggu 5

--- Android-Studio/RecycleViewCardView/app/src/main/java/com/riskyo/recycleviewcardview/MainActivity.java
+++ Android-Studio/RecycleViewCardView/app/src/main/java/com/riskyo/recycleviewcardview/MainActivity.java
@@ -1,14 +1,50 @@
 package com.riskyo.recycleviewcardview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ScrollView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    SiswaAdapter adapter;
+    List<Siswa> siswaList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        load();
+        isiData();
     }
+
+    public void load() {
+        recyclerView = findViewById(R.id.rcvSiswa);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    public void isiData() {
+        siswaList = new ArrayList<Siswa>();
+        siswaList.add(new Siswa("Joni", "Surabaya"));
+        siswaList.add(new Siswa("Eko", "Sidoarjo"));
+        siswaList.add(new Siswa("Fian", "Surakarta"));
+        siswaList.add(new Siswa("Jon", "Lumajang"));
+
+        adapter = new SiswaAdapter(this, siswaList);
+        recyclerView.setAdapter(adapter);
+    }
+
+    public void btnTambah(View view){
+        siswaList.add(new Siswa("Joni", "Surabaya"));
+        adapter.notifyDataSetChanged();
+    }
+
 }
